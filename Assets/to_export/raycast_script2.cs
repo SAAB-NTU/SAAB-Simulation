b1bd8ee65887c93445361987eafc803f3f46066d
zejie <zkong002@e.ntu.edu.sg>
2022-03-03 10:29:15 +0800

    added sonar

--- Assets/to_export/raycast_script2.cs
+++ Assets/to_export/raycast_script2.cs
@@ -7,14 +7,17 @@ using UnityEngine;
 public class raycast_script2 : MonoBehaviour
 {
     public GameObject prefab,coord,graph;
-    public float multiplier,scale;
+    public float multiplier,scale,offset,tot;
 
-    //List<float> vals;
+    List<float> vals,bef,aft;
     List<GameObject> coords;
     // Start is called before the first frame update
     void Start()
     {
-        //vals = new List<float>();
+        tot = 0;
+        vals = new List<float>();
+        bef = new List<float>();
+        aft = new List<float>();
         coords = new List<GameObject>();
         for (int i = 0; i < 5*multiplier; ++i)
         {
@@ -26,7 +29,9 @@ public class raycast_script2 : MonoBehaviour
             ray.transform.rotation=(Quaternion.Euler(0, value,0));
             GameObject coordinate = Instantiate(coord);
             coordinate.transform.SetParent(graph.transform);
-            coordinate.GetComponent<RectTransform>().anchoredPosition=new Vector2(i*495/ (5 * multiplier), 0);
+            vals.Add(value);
+            //coordinate.GetComponent<RectTransform>().eulerAngles = new Vector3(0,0,value);
+            //coordinate.GetComponent<RectTransform>().anchoredPosition=new Vector2(i*495/ (5 * multiplier), 0);
             
             
             coords.Add(coordinate);
@@ -35,14 +40,19 @@ public class raycast_script2 : MonoBehaviour
     }
     private void Update()
     {
-        
-        for(int i=0;i<transform.childCount;++i)
+        aft = new List<float>();
+        for (int i=0;i<transform.childCount;++i)
         {
             //vals.Add(transform.GetChild(i).GetComponent<raycast_script>().hit_val);
             try
             {
             Vector2 ini=coords[i].GetComponent<RectTransform>().anchoredPosition;
-            coords[i].GetComponent<RectTransform>().anchoredPosition = new Vector2(ini.x, scale*transform.GetChild(i).GetComponent<raycast_script>().hit_val);
+               
+                float r = scale * transform.GetChild(i).GetComponent<raycast_script>().hit_val;
+                float cos_theta = Mathf.Cos(Mathf.PI*(vals[i]/180));
+                float sin_theta = Mathf.Sin(Mathf.PI * (vals[i] / 180));
+                aft.Add(r);
+                coords[i].GetComponent<RectTransform>().anchoredPosition = new Vector2(offset+(r *cos_theta),Mathf.Abs(r * sin_theta));
             }
             catch(System.Exception e)
             {
@@ -51,9 +61,22 @@ public class raycast_script2 : MonoBehaviour
             }
             
         }
-       // print(vals.Count);
-       // vals = new List<float>();
-       // print(hits.Length);
+        if (bef.Count>0)
+        {
+            tot = 0;
+            for (int i = 0; i < aft.Count; ++i)
+            {
+                tot += (Mathf.Abs(aft[i] - bef[i]));
+            }
+            tot /= bef.Count;
+            tot /= Time.deltaTime;
+
+        }
+        bef = aft;
+        
+        // print(vals.Count);
+        // vals = new List<float>();
+        // print(hits.Length);
     }
     // Update is called once per frame
 
