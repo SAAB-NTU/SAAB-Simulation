32922008be16ed3bcd85b689a04963b39f27cc99
zejie <zkong002@e.ntu.edu.sg>
2022-03-23 14:00:40 +0800

    implemented sonar particle reflection, added layer in unity to avoid colliding between particles

--- Assets/Scripts/single_beam_test.cs
+++ Assets/Scripts/single_beam_test.cs
@@ -12,44 +12,36 @@ public class single_beam_test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Rigidbody rb = GetComponent<Rigidbody>();
-        //transform.Rotate(0,45,0);
-        rb.velocity = new Vector3(-Mathf.Cos(Mathf.PI/4),0,Mathf.Cos(Mathf.PI/4)) * velocity;
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         time_travelled += Time.deltaTime;
-        
-    
-       
-    
     }
 
     void OnCollisionEnter(Collision collision)
     {
+
         Debug.Log("collided");
         Rigidbody rb = GetComponent<Rigidbody>();
         normal_sph = collision.contacts[0].normal;
         Vector3 _velocity = Vector3.Reflect(rb.velocity,normal_sph);
         rb.velocity = _velocity ;
+
         
+        // // Print how many points are colliding with this transform
+        // Debug.Log("Points colliding: " + collision.contacts.Length);
 
- 
-        // Print how many points are colliding with this transform
-        Debug.Log("Points colliding: " + collision.contacts.Length);
+        // // Print the normal of the first point in the collision.
+        // Debug.Log("Normal of the first point: " + collision.contacts[0].normal);
 
-        // Print the normal of the first point in the collision.
-        Debug.Log("Normal of the first point: " + collision.contacts[0].normal);
-
-        // Draw a different colored ray for every normal in the collision
-        foreach (var item in collision.contacts)
-        {
-            Debug.DrawRay(item.point, item.normal * 100, Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f), 100f);
-        }
+        // // Draw a different colored ray for every normal in the collision
+        // foreach (var item in collision.contacts)
+        // {
+        //     Debug.DrawRay(item.point, item.normal * 100, Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f), 100f);
+        // }
     
-
-
     }
 }
--- Assets/Scripts/sonar.cs
+++ Assets/Scripts/sonar.cs
@@ -7,39 +7,49 @@ public class sonar : MonoBehaviour
     public GameObject preFab;
     public Vector3 normal_ray;
     public float sound_speed = 5f;
+    public float FOV = 30;
+    public float multiplier = 3;
 
     [SerializeField]
     float distance;
 
     // Start is called before the first frame update
-    GameObject beam;
-    void Awake()
+    void Start()
     {
-        beam = Instantiate(preFab,new Vector3(-0.3251f,0,0),Quaternion.identity);
-        beam.GetComponent<single_beam_test>().velocity = sound_speed;
+        float angle = FOV/(2);
+        for(int i = 0;i < multiplier;i++)
+        {
+            Debug.Log(i);
+            float x = -sound_speed * Mathf.Cos(Mathf.Deg2Rad*angle);
+            float y = 0;
+            float z = -sound_speed * Mathf.Sin(Mathf.Deg2Rad *angle);
+            GameObject beam = Instantiate(preFab);
+            beam.GetComponent<Rigidbody>().velocity = new Vector3(x,y,z);
+            angle -= FOV/(multiplier-1);
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-         //GetComponent<LineRenderer>().SetPosition(0, transform.position);
-        RaycastHit hit;
-        //Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out hit, 30))
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.left) * hit.distance, Color.green);
-            // print(hit.point.z);
-            //GetComponent<LineRenderer>().SetPosition(1, transform.TransformDirection(Vector3.forward) * hit.distance- transform.position);
-            //hit_val = hit.distance;
-            // Debug.Log("Did Hit");
-        }
-        else
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.left) * 30, Color.white);
-            // Debug.Log("Did not Hit");
-            //hit_val = -1;
-        }
-        normal_ray=hit.normal;
+        //  //GetComponent<LineRenderer>().SetPosition(0, transform.position);
+        // RaycastHit hit;
+        // //Does the ray intersect any objects excluding the player layer
+        // if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out hit, 30))
+        // {
+        //     Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.left) * hit.distance, Color.green);
+        //     // print(hit.point.z);
+        //     //GetComponent<LineRenderer>().SetPosition(1, transform.TransformDirection(Vector3.forward) * hit.distance- transform.position);
+        //     //hit_val = hit.distance;
+        //     // Debug.Log("Did Hit");
+        // }
+        // else
+        // {
+        //     Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.left) * 30, Color.white);
+        //     // Debug.Log("Did not Hit");
+        //     //hit_val = -1;
+        // }
+        // normal_ray=hit.normal;
     }
 
     void OnTriggerEnter(Collider other)
