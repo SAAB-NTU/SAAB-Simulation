dad55e29c4231383ba7f8d74b579a49da4ca6239
Ze Jie <ze-jie-kong-@hotmail.com>
2022-10-09 14:21:36 +0800

    csv logging for kalman filter test

--- Assets/Scripts/archives/read_val.cs
+++ Assets/Scripts/archives/read_val.cs
@@ -5,35 +5,59 @@ using UnityEngine;
 
 public class read_val : MonoBehaviour
 {
-    public GameObject IMU;
-    public Rigidbody Real;
-    public single_beam_old SONAR;
     // Start is called before the first frame update
-    public float predicted_velocity, real_velocity, measured_velocity,predicted_error,measured_error,Accepted_rays;
-
     
+    public GameObject cube;
+    public float sonar_distance,accel_x,accel_y,accel_z,angular_x,angular_y,angular_z;
+    int confidence;
+    float sonar_timer = 0;
+    float timer = 0;
+    float positionX,positionY,positionZ;
 
     void Start() //save to csv
     {
-        // var w = new StreamWriter("no_water(7).csv",true); //csv file saved to Asset folder
-        // var line = "predicted_velocity,real_velocity,measured_velocity,predicted_error,measured_error,accepted_rays";
-        // w.WriteLine(line);
-        // w.Close();
+        var w = new StreamWriter("kalman_filter_with_noise.csv",true); //csv file saved to Asset folder
+        var line = "time,sensor,sonar_distance,confidence,position x,position y,position z,accel x,accel y,angular_vel x,angular_vel y,angular_vel z";
+        w.WriteLine(line);
+        w.Close();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        predicted_velocity = Mathf.Abs((IMU.GetComponent<Estimated_Position>().estimated_velocity()).x); //for x direction only
-        real_velocity= Mathf.Abs(Real.velocity.x);
-        measured_velocity = Mathf.Abs(SONAR.tot);
-        predicted_error = 100*Mathf.Abs((real_velocity - predicted_velocity ) / real_velocity);
-        measured_error = 100 * Mathf.Abs((real_velocity - measured_velocity )/ real_velocity);
-        Accepted_rays=SONAR.k;
-        //save to csv
-        // var w = new StreamWriter("no_water(7).csv",true);
-        // var line = string.Format("{0},{1},{2},{3},{4},{5}",predicted_velocity,real_velocity,measured_velocity,predicted_error,measured_error,Accepted_rays);
-        // w.WriteLine(line);
-        // w.Close();
+        if (sonar_timer >= 0.05)
+        {
+            sonar_distance = cube.GetComponent<raycast_script>().hit_val;
+            confidence = cube.GetComponent<raycast_script>().confidence;
+            positionX = cube.transform.position.x;
+            positionY = cube.transform.position.y;
+            positionZ = cube.transform.position.z;
+            var w = new StreamWriter("kalman_filter_with_noise.csv",true);
+            var line = string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8}",timer,"sonar",sonar_distance,confidence,
+                                      positionX,positionY,positionZ,"","","","","","");
+            sonar_timer = 0;
+            w.WriteLine(line);
+            w.Close();
+        }
+
+        else
+        {
+            Vector3 acceleration = cube.GetComponent<imu_noise>().real_accel;
+            Vector3 angular_velocity = cube.GetComponent<imu_noise>().real_gyro;
+            accel_x = acceleration[0];
+            accel_y = acceleration[1];
+            accel_z = acceleration[2];
+            angular_x = angular_velocity[0];
+            angular_y = angular_velocity[1];
+            angular_z = angular_velocity[2];
+
+            var w = new StreamWriter("kalman_filter_with_noise.csv",true);
+            var line = string.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11}",timer,"imu","","","","","",
+                                    accel_x,accel_y,accel_z,angular_x,angular_y,angular_z);
+            w.WriteLine(line);
+            w.Close();
+            sonar_timer += Time.fixedDeltaTime;
+        }
+        timer += Time.fixedDeltaTime;
     }
 }
